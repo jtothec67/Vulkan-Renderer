da65f242ad7cc6b9bf7fd79556e44d84de89c2f5
jtothec67 <jamescomer03@gmail.com>
2026-09-05 13:14:22 +0100

    Added dependency setup
    Added Vulkan, Volk, GLFW, GLM, and Dear ImGui to the cmake list to be pulled from their respective repositories when building the project. Theoretically much simpler to build than how it was set up in JamesEngine, now just cmake -B build and open the .sln. Won't be all the libraries I will need but for now I can start coding.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,61 @@
+//TEST CODE
+
+#include <volk.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_vulkan.h>
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Vulkan Renderer\n";
+    IMGUI_CHECKVERSION();
 
+    ImGui::CreateContext();
+
+    std::cout << "ImGui version: " << ImGui::GetVersion() << '\n';
+
+    if (volkInitialize() != VK_SUCCESS)
+    {
+        std::cerr << "Failed to initialize Vulkan.\n";
+        return 1;
+    }
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW.\n";
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow* window = glfwCreateWindow(
+        1280,
+        720,
+        "Vulkan Renderer",
+        nullptr,
+        nullptr
+    );
+
+    if (!window)
+    {
+        glfwTerminate();
+        return 1;
+    }
+
+    glm::vec3 testVector(1.0f, 2.0f, 3.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    ImGui::DestroyContext();
     return 0;
 }
